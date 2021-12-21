be46aee37832c2e3df46b9a263b02daf626f3985
Peter <peter@ourstudio.se>
2021-12-21 15:42:06 +0100

    updates deps and webpack dev server settings

--- webpack.server.js
+++ webpack.server.js
@@ -3,13 +3,21 @@ const path = require("path");
 
 module.exports = {
   devServer: {
-    hot: true,
-    contentBase: path.resolve(__dirname, "build"),
-    port: 5000,
-    historyApiFallback: true,
-    writeToDisk: true,
     allowedHosts: process.env.WAYKE_HOST
       ? process.env.WAYKE_HOST.replace(/\s/g, "").split(",")
       : undefined,
+    historyApiFallback: true,
+    port: 5000,
+    client: {
+      overlay: false,
+    },
+    static: {
+      publicPath: path.resolve(__dirname, "build"),
+      serveIndex: true,
+    },
+    hot: "only",
+    devMiddleware: {
+      writeToDisk: true,
+    },
   },
 };
